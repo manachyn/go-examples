694b4a7a12965310a27f7ab36ed9f05e6810c68d
ivan.manachyn <ivan.manachyn@amadeus.com>
2017-10-23 17:41:02 +0300

    To public

---
+++ bookstore/models/db.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	_ "github.com/lib/pq"
+	"database/sql"
+)
+
+type Datastore interface {
+	AllBooks() ([]*Book, error)
+}
+
+type DB struct {
+	*sql.DB
+}
+
+func NewDB(dataSourceName string) (*DB, error) {
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return &DB{db}, nil
+}
